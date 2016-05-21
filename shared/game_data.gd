ad480a8fd355808ae4b0e87d2a24c15218da6b39
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2016-05-21 15:57:11 +0300

    Reorder files in main/ and stages/

---
+++ shared/game_data.gd
@@ -0,0 +1,59 @@
+# This file is part of the Jetpaca game
+# Copyright (c) 2009-2016 Juan Linietsky, Ariel Manzur
+# Distributed under the terms of the MIT license (cf. LICENSE.md file)
+
+extends Node
+
+class LevelInfo:
+	var id = Vector2()
+	var large_fruits = [false,false,false]
+	var max_fruit = 0
+	var preview_path = ""
+	var path = ""
+	var enabled = true
+
+var last_level = 0
+var last_world = 1
+var levels = []
+
+var current_checkpoint = ""
+var current_world = null
+var current_large_fruits = [false, false, false]
+var current_keys = [false, false, false]
+var life_count=5
+
+func _init():
+	var stage_list = preload("res://stages/stage_list.gd").get_stage_list()
+	for stage in stage_list:
+		var level = LevelInfo.new()
+		level.id = stage.id
+		level.path = stage.path + ".tscn"
+		level.preview_path= stage.path + ".png"
+		level.enabled = true
+		levels.push_back(level)
+		prints("Level:", level.id, "path:", level.path)
+
+func stage_reset():
+	life_count = 5
+	current_checkpoint = ""
+	current_world = null
+	current_large_fruits = []
+	current_keys = [false, false, false]
+
+func get_level(id):
+	for level in levels:
+		if level.id == id:
+			return level
+	return null
+
+func set_current_stage(path):
+	for level in levels:
+		if level.path == path:
+			current_world = level
+			return
+	print("Warning: World not found in list: ", path)
+	current_world = LevelInfo.new()
+	current_world.path = path
+
+func game_over():
+	stage_reset() # Todo: remove alias?
--- stages/stage_list.gd
+++ stages/stage_list.gd
@@ -6,11 +6,11 @@ static func get_stage_list():
 	
 	
 	var sl=[
-		{ "id":Vector2(1,1), "path":"res://stages/intro" },
-		{ "id":Vector2(1,2), "path":"res://stages/forest_fun" },
-		{ "id":Vector2(1,3), "path":"res://stages/roller_coaster" },
-		{ "id":Vector2(1,5), "path":"res://stages/demo_gcon_004" },
-		{ "id":Vector2(1,7), "path":"res://stages/demo_gcon_006" }
+		{ "id":Vector2(1,1), "path":"res://stages/world_1/intro" },
+		{ "id":Vector2(1,2), "path":"res://stages/world_1/forest_fun" },
+		{ "id":Vector2(1,3), "path":"res://stages/world_1/roller_coaster" },
+		{ "id":Vector2(1,5), "path":"res://stages/gcon/demo_gcon_004" },
+		{ "id":Vector2(1,7), "path":"res://stages/gcon/demo_gcon_006" }
 	]
 	
 	return sl
